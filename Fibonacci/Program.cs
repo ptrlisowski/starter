69a91a0ce4bacd3cdff7063a0125bac4121c13e4
ptrlisowski <lisowski.ptr@gmail.com>
2017-03-24 12:18:34 +0100

    Fibonacci sequence. C#
    Finding specified number of Fibonacci sequence characters. Console application made with C#

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int sum = 1;
+
+            Console.WriteLine("How many Fibonacci numbers do you want?");
+            float leng = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < leng; i++)
+            {
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+                Console.Write(sum + " ");
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
+
