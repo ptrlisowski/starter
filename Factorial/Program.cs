9e8186cb4e72974fa0d6a8dd78281d7cc4c2bf25
ptrlisowski <lisowski.ptr@gmail.com>
2017-03-24 12:23:13 +0100

    Factorial calculator. C#
    Simple logic for calculating factorials of given number. Console application made with C#

---
+++ Factorial/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace silnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Silnia();
+        }
+
+        static void Silnia()
+        {
+            long number;
+            long i, f, n;
+
+
+            Console.WriteLine("\nEnter a valid number to calculate its factorial");
+
+            if (!long.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Entered characters are not a valid number");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+
+            if (number == 0)
+            {
+                Console.WriteLine("Factorial for " + number + " (" + number + "!)" + " equals 1");
+            }
+
+            i = 1;
+            n = 1;
+            f = 1;
+
+
+            while (i <= number)
+            {
+                f = n * i;
+
+                n = f;
+                i++;
+            }
+
+            Console.WriteLine("Factorial for " + number + " (" + number + "!)" + " equals " + f);
+            Console.WriteLine("Press 'r' to restart or anything else twice to quit");
+            Reboot();
+        }
+
+        static void Reboot()
+        {
+            var info = Console.ReadKey();
+            if (info.Key == ConsoleKey.R)
+            {
+                Silnia();
+            }
+        }
+    }
+}
